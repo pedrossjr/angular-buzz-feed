4e1aee4fef33fc7724f4f86e41f522417683e0d6
pedrossjr <pedrossjr@yahoo.com.br>
2025-10-09 12:13:45 -0300

    Atualização de projeto

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-quizz',
@@ -7,6 +7,36 @@ import { Component } from '@angular/core';
   templateUrl: './quizz.component.html',
   styleUrl: './quizz.component.css'
 })
-export class QuizzComponent {
+
+export class QuizzComponent implements OnInit {
+
+  title:string = "";
+
+  questions:any;
+  questionSelected:any;
+
+  answers:string[] = [];
+  answerSelected:string = "";
+
+  questionIndex:number = 0;
+  questionMaxIndex:number = 0;
+
+  finished:boolean = false;
+
+  constructor() {}
+
+  ngOnInit(): void {
+
+
+    /*
+    this.questionIndex = 0;
+    this.finished = false;
+    this.title = quizz_questions.title;
+
+    this.questions = quizz_questions.questions;
+    this.questionSelected = this.questions[this.questionIndex];
+    this.questionMaxIndex = this.questions.length - 1;
+    */
+  }
 
 }
